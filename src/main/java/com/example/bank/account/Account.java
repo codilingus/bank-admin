2f209c86c14d92ca471e834d6e632de890d57c77
Aleksander <aleksanderkotowicz@gmail.com>
2018-04-21 09:57:48 +0200

    User and account created

--- src/main/java/com/example/bank/account/Account.java
+++ src/main/java/com/example/bank/account/Account.java
@@ -1,4 +1,15 @@
 package com.example.bank.account;
 
+import java.time.LocalDate;
+
 public class Account {
+
+    private int accountId;
+    private double balance;
+    private LocalDate creationDate;
+
+    public Account(int accountId) {
+        this.accountId = accountId;
+        this.creationDate = LocalDate.now();
+    }
 }
--- src/main/java/com/example/bank/user/User.java
+++ src/main/java/com/example/bank/user/User.java
@@ -1,10 +1,31 @@
 package com.example.bank.user;
 
+import com.example.bank.account.Account;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class User {
 
+    private int userId;
     private String name;
     private String surname;
     private int pesel;
+    private List<Account> usersAccount = new LinkedList<>();
+
+    public User(String name, String surname, int pesel) {
+        this.name = name;
+        this.surname = surname;
+        this.pesel = pesel;
+    }
+
+    public User() {
+    }
+
+    public void addAccount(Account account) {
+        usersAccount.add(account);
+    }
+
 
 
 }
