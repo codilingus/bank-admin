09a0f65ef5df252b7b604d107ddde5ac29e05634
Marcin <jmarcin511@gmail.com>
2018-04-21 08:44:15 +0200

    initial-structure

---
+++ src/main/java/com/example/bank/account/Account.java
@@ -0,0 +1,4 @@
+package com.example.bank.account;
+
+public class Account {
+}
---
+++ src/main/java/com/example/bank/user/User.java
@@ -0,0 +1,10 @@
+package com.example.bank.user;
+
+public class User {
+
+    private String name;
+    private String surname;
+    private int pesel;
+
+
+}
